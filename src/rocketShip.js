f64826a1bbde1af3a5012b939079247d577fbeed
Joe Davis <jdavis@grayshift.com>
2019-12-30 12:09:30 -0600

    steer rocket toward cursor

--- src/rocketShip.js
+++ src/rocketShip.js
@@ -8,8 +8,9 @@ ship.src = shipImg;
 
 const rocketSize = 50;
 // const acceleration = 5;
-// const topSpeed = 10;
-const turnSpeed = 180;
+// const maxVelocity = 10;
+const maxRotationalVelocity = 120;
+const rotationalAcceleration = 360;
 
 let x = Math.floor(window.innerWidth / 2);
 let y = Math.floor(window.innerHeight / 2);
@@ -17,7 +18,8 @@ let cursorX = x;
 let cursorY = y;
 // let xVel = 0;
 // let yVel = 0;
-let rotation = 0;
+let rotationalVelocity = 0;
+let rotation = 60;
 let timestamp = new Date().getTime();
 
 function init() {
@@ -29,34 +31,103 @@ function init() {
     cursorY = e.clientY;
   });
 
+  ctx.font = '18px serif';
+
   window.requestAnimationFrame(draw);
 }
 
+const radiansToDegrees = (radians) => radians * (180 / Math.PI);
+
+const determineAngleToDestination = () => {
+  const xComponent = cursorX - x;
+  const yComponent = cursorY - y;
+  const hypot = Math.sqrt(Math.pow(xComponent, 2) + Math.pow(yComponent, 2));
+  if (xComponent >= 0 && yComponent < 0) {
+    // quadrant 1
+    return radiansToDegrees(Math.asin(xComponent / hypot));
+  }
+  if (xComponent >= 0 && yComponent >= 0) {
+    // quadrant 2
+    return 180 - radiansToDegrees(Math.asin(xComponent / hypot));
+  }
+  if (xComponent < 0 && yComponent >= 0) {
+    // quadrant 3
+    return 180 + Math.abs(radiansToDegrees(Math.asin(xComponent / hypot)));
+  }
+  if (xComponent < 0 && yComponent < 0) {
+    // quadrant 4
+    return 360 + radiansToDegrees(Math.asin(xComponent / hypot));
+  }
+  return 0;
+};
+
+const determineAngleCorrection = () => {
+  const destinationAngle = determineAngleToDestination();
+  const diff = ((destinationAngle - rotation + 180) % 360) - 180;
+  return diff < -180 ? diff + 360 : diff;
+};
+
 function draw() {
-  ctx.fillStyle = 'rgba(255, 255, 255, 1)';
+  const correction = determineAngleCorrection();
+
+  // Clear canvas
+  ctx.fillStyle = 'rgba(255, 255, 255, 0.3)';
   ctx.fillRect(0, 0, canvas.width, canvas.height);
 
+  // Debug
+  ctx.save();
+  ctx.fillStyle = 'rgba(0, 0, 0, 1)';
+  ctx.fillText(`Rotational Velocity: ${rotationalVelocity}`, 10, 20);
+  ctx.fillText(`Angular Correction: ${correction}`, 10, 40);
+  ctx.restore();
+
+  // Determine time modifier
   const newTime = new Date().getTime();
   const elapsed = newTime - timestamp;
   timestamp = newTime;
   const timeModifier = elapsed / 1000;
 
-  rotation = rotation + turnSpeed * timeModifier;
-  if (rotation >= 360) {
-    rotation = rotation - 360;
-  }
-  if (rotation < 0) {
-    rotation = rotation + 360;
+  // Steer ship
+
+  const rotationalVelocityDelta = rotationalAcceleration * timeModifier;
+  if (correction) {
+    if (correction > 0) {
+      // turn right
+      // if (rotationalVelocity > correction) {
+      //   rotationalVelocity -= rotationalVelocityDelta;
+      // } else {
+      rotationalVelocity += rotationalVelocityDelta;
+      // }
+      rotationalVelocity = Math.min(rotationalVelocity, maxRotationalVelocity);
+    }
+    if (correction < 0) {
+      // turn left
+      // if (rotationalVelocity < correction) {
+      //   rotationalVelocity += rotationalVelocityDelta;
+      // } else {
+      rotationalVelocity -= rotationalVelocityDelta;
+      // }
+      rotationalVelocity -= rotationalVelocityDelta;
+      rotationalVelocity = Math.max(
+        rotationalVelocity,
+        maxRotationalVelocity * -1,
+      );
+    }
+    rotation = rotation + rotationalVelocity * timeModifier;
   }
 
-  // x += xVel;
-  // y += yVel;
+  // rotation = rotation + maxRotationalVelocity * timeModifier;
+  // if (rotation >= 360) {
+  //   rotation = rotation - 360;
+  // }
+  // if (rotation < 0) {
+  //   rotation = rotation + 360;
+  // }
 
+  // Draw rocket
   ctx.save();
   ctx.translate(x, y);
   ctx.rotate((rotation * Math.PI) / 180);
-
-  // Draw rocket
   ctx.drawImage(
     ship,
     0 - rocketSize / 2,
